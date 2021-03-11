a35b519a24cf030e9b3587b3241e904cae0b4af8
cancio <filipe.cancio@outlook.com>
2021-03-11 14:58:43 -0300

    #2 Criação de activity e tela de login

---
+++ app/src/main/java/dev/cancio/firebasecursosf/AberturaActivity.kt
@@ -0,0 +1,11 @@
+package dev.cancio.firebasecursosf
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AberturaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_abertura)
+    }
+}
